a4d2cce81d870159add39cf799a797064822dab6
yutian929 <yutian929@outlook.com>
2025-04-09 15:52:36 +0800

    yanbot, initial.

--- checkpoints/download_ckpts.sh
+++ checkpoints/download_ckpts.sh
@@ -44,16 +44,16 @@ sam2p1_hiera_b_plus_url="${SAM2p1_BASE_URL}/sam2.1_hiera_base_plus.pt"
 sam2p1_hiera_l_url="${SAM2p1_BASE_URL}/sam2.1_hiera_large.pt"
 
 # SAM 2.1 checkpoints
-echo "Downloading sam2.1_hiera_tiny.pt checkpoint..."
-$CMD $sam2p1_hiera_t_url || { echo "Failed to download checkpoint from $sam2p1_hiera_t_url"; exit 1; }
+# echo "Downloading sam2.1_hiera_tiny.pt checkpoint..."
+# $CMD $sam2p1_hiera_t_url || { echo "Failed to download checkpoint from $sam2p1_hiera_t_url"; exit 1; }
 
-echo "Downloading sam2.1_hiera_small.pt checkpoint..."
-$CMD $sam2p1_hiera_s_url || { echo "Failed to download checkpoint from $sam2p1_hiera_s_url"; exit 1; }
+# echo "Downloading sam2.1_hiera_small.pt checkpoint..."
+# $CMD $sam2p1_hiera_s_url || { echo "Failed to download checkpoint from $sam2p1_hiera_s_url"; exit 1; }
 
 echo "Downloading sam2.1_hiera_base_plus.pt checkpoint..."
 $CMD $sam2p1_hiera_b_plus_url || { echo "Failed to download checkpoint from $sam2p1_hiera_b_plus_url"; exit 1; }
 
-echo "Downloading sam2.1_hiera_large.pt checkpoint..."
-$CMD $sam2p1_hiera_l_url || { echo "Failed to download checkpoint from $sam2p1_hiera_l_url"; exit 1; }
+# echo "Downloading sam2.1_hiera_large.pt checkpoint..."
+# $CMD $sam2p1_hiera_l_url || { echo "Failed to download checkpoint from $sam2p1_hiera_l_url"; exit 1; }
 
 echo "All checkpoints are downloaded successfully."
